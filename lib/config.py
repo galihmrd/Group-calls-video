7b9c5b326ba6b5aac3ce7fd30d897ab9e2ce6adb
Imszy17 <galihmardiana15@gmail.com>
2021-09-05 04:34:42 +0000

    command: add userbot join

--- lib/config.py
+++ lib/config.py
@@ -1,7 +1,8 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SESSION_NAME = os.getenv("SESSION_NAME")
 USERNAME_BOT = os.getenv("USERNAME_BOT")
+UBOT_ID = os.getenv("UBOT_ID")
---
+++ lib/driver/join.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.errors import UserAlreadyParticipant
+from lib.driver.stream import app as USER
+from lib.config import USERNAME_BOT, UBOT_ID
+
+
+@Client.on_message(filters.command(["join", "join@{USERNAME_BOT}"]))
+async def join(client, message):
+    chat_id = message.chat.id
+    try:
+        link = await client.export_chat_invite_link(chat_id)
+    except BaseException:
+        await message.reply("**Error:**\nAdd me as admin of your group!")
+        return
+    try:
+        await client.unban_chat_member(chat_id, int(UBOT_ID))
+        await USER.join_chat(link)
+    except UserAlreadyParticipant:
+        pass
