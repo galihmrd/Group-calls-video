bf7d79d2e0f543de9fc4a025470fcc765f1e4803
Imszy17 <galihmardiana15@gmail.com>
2021-09-05 14:46:58 +0000

    ytstream: cleanup and improvements

--- lib/driver/ytstream.py
+++ lib/driver/ytstream.py
@@ -9,10 +9,11 @@ VIDEO_CALL = {}
 @Client.on_message(filters.command("ytstream"))
 async def ytstream(client, message):
     query = message.command[1]
+    rby = message.from_user.mention
     chat_id = message.chat.id
     video = pafy.new(query)
+    txt = await message.reply(f"```Converting url...```\nUrl: ```{query}```")
     final_source = video.getbest().url
-    txt = await message.reply("```Download and converting...```")
     if len(message.command) < 2:
         await message.reply("Give some youtube video url")
     else:
@@ -20,14 +21,14 @@ async def ytstream(client, message):
         await group_call.join(chat_id)
         await group_call.start_video(final_source)
         VIDEO_CALL[chat_id] = group_call
-        await message.reply("**Streamed!**")
+        await message.reply(f"**Streaming via youtube url**\n**Requested by:** {rby}\n**To stop:** /ytstop")
         await txt.delete()
 
-@Client.on_message(filters.command("ytstop"))
+@Client.on_message(filters.command(["ytstop", "ytstop@{USERNAME_BOT}"]))
 async def ytstop(client, message):
     chat_id = message.chat.id
     try:
         await VIDEO_CALL[chat_id].stop()
-        await message.reply("**Stoped!**")
+        await message.reply("**Stopped!**")
     except Exception as e:
         await message.reply("error")
