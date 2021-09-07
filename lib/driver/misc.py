80a4ddff59e2c5e739db63fbf06ee96aa32cf596
Imszy17 <galihmardiana15@gmail.com>
2021-09-07 11:38:49 +0000

    command: add stop schedule cmd and merge /cstop -> /stop command: /schedule 10

--- lib/driver/misc.py
+++ lib/driver/misc.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import datetime
 from pyrogram.types import Message
 from pyrogram import Client, filters
@@ -8,25 +9,17 @@ VIDEO_CALL = {}
 CHANNEL_VIDEO = {}
 
 
-@Client.on_message(filters.command(["stop", "stop@{USERNAME_BOT}"]))
-async def stopvideo(client, m: Message):
-    chat_id = m.chat.id
-    user = m.from_user.mention
-    try:
-        await VIDEO_CALL[chat_id].stop()
-        await m.reply(f"**Stopped by {user}!**")
-    except Exception as e:
-        await m.reply(f"**Error** - `{e}`")
-
-@Client.on_message(filters.command(["cstop", "cstop@{USERNAME_BOT"]))
+@Client.on_message(filters.command(["stop", "stop@{USERNAME_BOT"]))
 async def cstop(client, message):
     chat_id = message.chat.title
+    chid = message.chat.id
     user = message.from_user.mention
     try:
-        await CHANNEL_VIDEO[chat_id].stop()
         await message.reply(f"**Stopped by {user}!**")
+        await VIDEO_CALL[chid].stop()
+        await CHANNEL_VIDEO[chat_id].stop()
     except Exception as e:
-        await message.reply(f"**Error:** `{e}`")
+        pass
 
 @Client.on_message(filters.command(["ping", "ping@{USERNAME_BOT}"]))
 async def ping_(client: Client, message: Message):
@@ -40,3 +33,18 @@ async def ping_(client: Client, message: Message):
 async def repo(client, message):
     repo = "https://github.com/Imszy17/tg-stream-video"
     await message.reply(f"**Source code:** [Here]({repo})")
+
+@Client.on_message(filters.command("schedule"))
+async def sch(client, message):
+    chat_id = message.chat.id
+    chid = message.chat.title
+    user = message.from_user.mention
+    input = message.command[1]
+    txt = await message.reply(f"**Stopped in** `{input}s`")
+    await asyncio.sleep(int(input))
+    try:
+        await txt.edit(f"**Stopped by {user}!**")
+        await VIDEO_CALL[chat_id].stop()
+        await CHANNEL_VIDEO[chid].stop()
+    except Exception as e:
+        pass
--- lib/driver/ytstream.py
+++ lib/driver/ytstream.py
@@ -72,7 +72,7 @@ async def cstream(client, message):
             ],
         )
         await message.reply_photo(
-            caption=f"**Streaming via [youtube url]({query})**\n**Requested by:** {rby}\n**To stop:** /cstop",
+            caption=f"**Streaming via [youtube url]({query})**\n**Requested by:** {rby}\n**To stop:** /stop",
             photo="./etc/banner.png",
             reply_markup=keyboard,
         )
