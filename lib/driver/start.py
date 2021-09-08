669809d642d0332b51e5fc7f6179fc1fd68fa2c4
Imszy17 <galihmardiana15@gmail.com>
2021-09-08 02:45:15 +0000

    command: add help command

--- lib/driver/start.py
+++ lib/driver/start.py
@@ -4,6 +4,24 @@ from pyrogram import Client, filters
 from lib.config import USERNAME_BOT
 
 
+HELP_TEXT = """**List Command:**
+
+**/stream** -> [reply to file/put streaming url]
+**/cstream** -> [stream in channel]
+
+**/ytstream** -> [put youtube url]
+**/ytcstream** -> [stream in channel]
+
+**/stop** -> [for group]
+**/cstop** -> [for channel]
+
+**/schedule** {value} -> [stop scheduler]
+"""
+
 @Client.on_message(filters.command(["start", "start@{USERNAME_BOT}"]))
 async def start(client, message):
     await message.reply("**👋 I'm alive**")
+
+@Client.on_message(filters.command(["help", "help@{USERNAME_BOT}"]))
+async def help(client, message):
+    await message.reply(HELP_TEXT)
