663988c2b8a5421acaa4887007b5d983044c5b7b
Imszy17 <galihmardiana15@gmail.com>
2021-09-16 13:52:58 +0000

    media_downloader: remove unused code

--- lib/driver/media_downloader.py
+++ lib/driver/media_downloader.py
@@ -22,16 +22,10 @@ async def video(client, message):
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
         link = f"https://youtube.com{results[0]['url_suffix']}"
-        # print(results)
         title = results[0]["title"][:40]
         thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"thumb{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
         duration = results[0]["duration"]
         results[0]["url_suffix"]
-        results[0]["views"]
-        rby = message.from_user.mention
     except Exception as e:
         print(e)
     try:
@@ -63,13 +57,8 @@ async def music(client, message):
         link = f"https://youtube.com{results[0]['url_suffix']}"
         title = results[0]["title"][:40]
         thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"thumb{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
-
         duration = results[0]["duration"]
         results[0]["url_suffix"]
-        results[0]["views"]
     except Exception as e:
         await message.reply("{str(e)}")
     msg = await message.reply("```Downloading...```")
