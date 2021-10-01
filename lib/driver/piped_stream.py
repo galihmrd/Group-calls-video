35d68f07eab4e3a1931a74b48368bbe2b5d42508
galihmrd <galihmardiana15@gmail.com>
2021-10-01 06:15:57 +0000

    piped_stream: added AudioImagePiped to stream audio file from telegram

--- lib/driver/piped_stream.py
+++ lib/driver/piped_stream.py
@@ -9,6 +9,7 @@ from lib.helpers.filters import private_filters, public_filters
 from pytgcalls import idle
 from pytgcalls import StreamType
 from pytgcalls.types.input_stream import AudioVideoPiped
+from pytgcalls.types.input_stream import AudioImagePiped
 from pytgcalls.types.input_stream.quality import MediumQualityAudio
 from pytgcalls.types.input_stream.quality import MediumQualityVideo
 
@@ -66,6 +67,24 @@ async def play_video(client, message):
                 MediumQualityVideo()
             ),
             stream_type=StreamType().live_stream
+        )
+    elif replied.audio:
+        flags =  " ".join(message.command[1:])
+        if flags == "channel":
+             chat_id = message.chat.title
+        else:
+             chat_id = message.chat.id
+        msg = await message.reply("```Downloading from telegram...```")
+        input_file = await client.download_media(replied)
+        await msg.edit("```Streamed```")
+        await call_py.join_group_call(
+            chat_id,
+            AudioImagePiped(
+                input_file,
+                './etc/banner.png',
+                video_parameters=MediumQualityVideo(),
+            ),
+            stream_type=StreamType().pulse_stream,
         )
     else:
         await message.reply("```Please reply to video or video file to stream```")
