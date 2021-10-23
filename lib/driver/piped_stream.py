0e641c3cbf8cd66de5503e13f759704d265e2b42
GalihMrd <69835093+galihmrd@users.noreply.github.com>
2021-10-24 04:41:33 +0700

    piped_stream: fix auto start group call (#39)
    * piped_stream: fix auto start group call
    
    * piped_stream: add message dialog auto start group call

--- lib/driver/piped_stream.py
+++ lib/driver/piped_stream.py
@@ -51,7 +51,8 @@ async def play_video(client, message):
                 stream_type=StreamType().live_stream
             )
         except NoActiveGroupCall:
-            opengc(client, message)
+            await msg.edit("**No active call!**\n```Starting Group call...```")
+            await opengc(client, message)
     elif replied.video or replied.document:
         flags = " ".join(message.command[1:])
         chat_id = int(
@@ -69,7 +70,8 @@ async def play_video(client, message):
                 stream_type=StreamType().live_stream
             )
         except NoActiveGroupCall:
-            opengc(client, message)
+            await msg.edit("**No active call!**\n```Starting Group call...```")
+            await opengc(client, message)
     elif replied.audio:
         flags = " ".join(message.command[1:])
         chat_id = int(
@@ -89,7 +91,8 @@ async def play_video(client, message):
                 stream_type=StreamType().pulse_stream,
             )
         except NoActiveGroupCall:
-            opengc(client, message)
+            await msg.edit("**No active call!**\n```Starting Group call...```")
+            await opengc(client, message)
     else:
         await message.reply("Error!")
 
