ba020d9f9554b3830d1d11a0eb33d5fd815b35a2
Sourcery AI <>
2021-10-23 11:38:36 +0000

    'Refactored by Sourcery'

--- lib/driver/broadcast.py
+++ lib/driver/broadcast.py
@@ -32,11 +32,8 @@ async def broadcast(client: Client, message: Message):
 @Client.on_message(filters.command("chatlist"))
 @sudo_users
 async def chatlist(client, message):
-    chats = []
     all_chats = load_chats_list()
-    for i in all_chats:
-        if str(i).startswith("-"):
-            chats.append(i)
+    chats = [i for i in all_chats if str(i).startswith("-")]
     chatfile = "Daftar chat.\n0. ID grup | Jumlah anggota | tautan undangan\n"
     P = 1
     for chat in chats:
@@ -50,7 +47,7 @@ async def chatlist(client, message):
             members = "Null"
         try:
             chatfile += "{}. {} | {} | {}\n".format(P, chat, members, link)
-            P = P + 1
+            P += 1
         except BaseException:
             pass
     with BytesIO(str.encode(chatfile)) as output:
