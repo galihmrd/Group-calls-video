763be9a7feb07f35cf7556474a32cec84e704951
Sourcery AI <>
2021-10-23 05:20:43 +0000

    'Refactored by Sourcery'

--- lib/driver/broadcast.py
+++ lib/driver/broadcast.py
@@ -26,7 +26,6 @@ async def broadcast(client: Client, message: Message):
         except:
             failed += 1
             remove_chat_from_db(str(chat))
-            pass
     await message.reply(
         f"Message sent to {success} chat(s). {failed} chat(s) failed recieve message"
     )
@@ -35,11 +34,8 @@ async def broadcast(client: Client, message: Message):
 @Client.on_message(filters.command("chatlist"))
 @sudo_users
 async def chatlist(client, message):
-    chats = []
     all_chats = load_chats_list()
-    for i in all_chats:
-        if str(i).startswith("-"):
-            chats.append(i)
+    chats = [i for i in all_chats if str(i).startswith("-")]
     chatfile = "Daftar chat.\n0. ID grup | Jumlah anggota | tautan undangan\n"
     P = 1
     for chat in chats:
@@ -53,7 +49,7 @@ async def chatlist(client, message):
             members = "Null"
         try:
             chatfile += "{}. {} | {} | {}\n".format(P, chat, members, link)
-            P = P + 1
+            P += 1
         except:
             pass
     with BytesIO(str.encode(chatfile)) as output:
