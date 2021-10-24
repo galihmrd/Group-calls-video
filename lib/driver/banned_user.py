2258784a8d2f164920c7f4c597cb786ef7115ac1
galihmrd <galihmardiana15@gmail.com>
2021-10-24 15:50:04 +0700

    improve banned user plugins

--- lib/driver/banned_user.py
+++ lib/driver/banned_user.py
@@ -12,22 +12,34 @@ from lib.helpers.decorators import sudo_users
 @Client.on_message(filters.command("gbl"))
 @sudo_users
 async def blacklist(client: Client, message: Message):
+    arg = message.text.split(None, 2)[1:]
     if message.reply_to_message:
-        user_id = message.reply_to_message.from_user["id"]
+        user_id = message.reply_to_message.from_user.id
+        try:
+           reason = arg[0]
+        except:
+           reason = "No reason"
     else:
-        arg = " ".join(message.command[1:])
-        if arg.startswith("@"):
+        if arg[0].startswith("@"):
             try:
-                user = await client.get_users(arg)
+                user = await client.get_users(arg[0])
                 user_id = user.id
+                try:
+                   reason = arg[1]
+                except:
+                   reason = "No reason"
             except BadRequest as ex:
                 await message.reply("not a valid user")
                 print(ex)
                 return ""
         else:
-            user_id = int(arg)
+            user_id = int(arg[0])
+            try:
+               reason = arg[1]
+            except:
+               reason = "No reason"
         db.banned_user(int(user_id))
-        await message.reply(f"[blacklisted](tg://user?id={user_id})")
+        await message.reply(f"**User:** [blacklisted](tg://user?id={user_id})\n**Reason:** {reason}")
 
 
 @Client.on_message(filters.command("ungbl"))
