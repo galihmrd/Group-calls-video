cc7ef238e862651041a43849177bdfe031c2e993
GalihMrd <69835093+galihmrd@users.noreply.github.com>
2021-10-24 19:16:52 +0700

    lib: driver: banned_user: just reply message from user to add blacklist (#44)
    * fix
    
    * 'Refactored by Sourcery' (#45)
    
    Co-authored-by: Sourcery AI <>
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- lib/driver/banned_user.py
+++ lib/driver/banned_user.py
@@ -13,40 +13,41 @@ from lib.helpers.decorators import sudo_users
 @sudo_users
 async def blacklist(client: Client, message: Message):
     arg = message.text.split(None, 2)[1:]
-    if message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
+    replied = message.reply_to_message
+    if replied:
+        user_id = replied.from_user.id
         try:
            reason = arg[0]
         except:
            reason = "No reason"
-    else:
-        if arg[0].startswith("@"):
-            try:
-                user = await client.get_users(arg[0])
-                user_id = user.id
-                try:
-                   reason = arg[1]
-                except:
-                   reason = "No reason"
-            except BadRequest as ex:
-                await message.reply("not a valid user")
-                print(ex)
-                return ""
-        else:
-            user_id = int(arg[0])
+    elif arg[0].startswith("@"):
+        try:
+            user = await client.get_users(arg[0])
+            user_id = user.id
             try:
                reason = arg[1]
             except:
                reason = "No reason"
-        db.banned_user(int(user_id))
-        await message.reply(f"**User:** [blacklisted](tg://user?id={user_id})\n**Reason:** {reason}")
+        except BadRequest as ex:
+            await message.reply("not a valid user")
+            print(ex)
+            return ""
+    else:
+        user_id = int(arg[0])
+        try:
+           reason = arg[1]
+        except:
+           reason = "No reason"
+    db.banned_user(int(user_id))
+    await message.reply(f"**User:** [blacklisted](tg://user?id={user_id})\n**Reason:** {reason}")
 
 
 @Client.on_message(filters.command("ungbl"))
 @sudo_users
 async def unblacklist(client: Client, message: Message):
-    if message.reply_to_message:
-        user_id = message.reply_to_message.from_user["id"]
+    replied = message.reply_to_message
+    if replied:
+        user_id = replied.from_user.id
     else:
         arg = " ".join(message.command[1:])
         if arg.startswith("@"):
@@ -58,5 +59,6 @@ async def unblacklist(client: Client, message: Message):
                 return ""
         else:
             user_id = int(arg)
-        db.unban_user(int(user_id))
-        await message.reply(f"[unblacklisted](tg://user?id={user_id})")
+
+    db.unban_user(int(user_id))
+    await message.reply(f"[unblacklisted](tg://user?id={user_id})")
