9533d242475f06754d2563f0ebc2e88870b075b0
GalihMrd <69835093+galihmrd@users.noreply.github.com>
2021-11-09 20:45:35 +0700

    Added youtube search module (#66)
    * 'Refactored by Sourcery' (#67)
    
    Co-authored-by: Sourcery AI <>
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

---
+++ lib/driver/yt_search.py
@@ -0,0 +1,26 @@
+from pyrogram.types import Message
+from pyrogram import Client, filters
+from youtube_search import YoutubeSearch
+from lib.helpers.decorators import blacklist_users
+
+
+@Client.on_message(filters.command("search"))
+@blacklist_users
+async def ytsearch(client, message):
+    try:
+        if len(message.command) <2:
+            await message.reply("Give me some title")
+            return
+        input = " ".join(message.command[1:])
+        msg = await message.reply("`searching...`")
+        results = YoutubeSearch(input, max_results=4).to_dict()
+        text = " "
+        for i in range(4):
+            text += f"**Title:** {results[i]['title']}\n"
+            text += f"**Duration:** {results[i]['duration']}\n"
+            text += f"**Views:** {results[i]['views']}\n"
+            text += f"**Channel:** {results[i]['channel']}\n"
+            text += f"**Url:** [Click Here](https://youtube.com{results[i]['url_suffix']})\n\n"
+        await msg.edit(text, disable_web_page_preview=True)
+    except BaseException:
+       pass
