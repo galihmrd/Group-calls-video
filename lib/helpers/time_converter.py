c395e9edf011ecdc71395cefd8effa3fa58d044b
galihmrd <galihmardiana15@gmail.com>
2022-01-31 22:31:43 +0000

    Auto fix: Code formatted

--- lib/helpers/time_converter.py
+++ lib/helpers/time_converter.py
@@ -1,3 +1,3 @@
 def cvt_time(time):
     stringt = str(time)
-    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
