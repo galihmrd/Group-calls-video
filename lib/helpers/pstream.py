867bba5a7d1dc9148133071fe357024a4e67e104
galihmrd <galihmardiana15@gmail.com>
2022-06-25 09:39:45 +0700

    Revert "Check group_call first and stop if it is_connected"
    This reverts commit 9de35a9b6d90eada2fa44600945c293c61c8124b.

--- lib/helpers/pstream.py
+++ lib/helpers/pstream.py
@@ -1,4 +1,3 @@
-import asyncio
 import os
 
 from lib.driver.misc import PAUSE, RESUME, STOP
@@ -9,36 +8,19 @@ group_call = group_call_factory.get_group_call()
 
 async def pstream(chat_id, file, audio=None):
     if audio:
-        if not group_call.is_connected:
-            await group_call.join(chat_id)
-        else:
-            await group_call.stop()
-            await asyncio.sleep(2)
-            await group_call.join(chat_id)
+        await group_call.join(chat_id)
         await group_call.start_video("./etc/banner.png", with_audio=False)
         await group_call.start_audio(file)
     else:
-        if not group_call.is_connected:
-            await group_call.join(chat_id)
-        else:
-            await group_call.stop()
-            await asyncio.sleep(2)
-            await group_call.join(chat_id)
-        await group_call.start_video(
-            file, enable_experimental_lip_sync=True, repeat=False
-        )
+        await group_call.join(chat_id)
+        await group_call.start_video(file)
     STOP[chat_id] = group_call
     PAUSE[chat_id] = group_call
     RESUME[chat_id] = group_call
 
 
 async def pstream_audio(chat_id, file, thumb):
-    if not group_call.is_connected:
-        await group_call.join(chat_id)
-    else:
-        await group_call.stop()
-        await asyncio.sleep(2)
-        await group_call.join(chat_id)
+    await group_call.join(chat_id)
     await group_call.start_video(thumb, with_audio=False)
     await group_call.start_audio(file)
     STOP[chat_id] = group_call
