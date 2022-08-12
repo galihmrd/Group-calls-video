d1019379ecc0197ad5970f555cad380c5559c95e
galihmrd <galihmardiana15@gmail.com>
2022-08-12 08:49:14 +0000

    Auto fix: Code formatted

--- lib/driver/combot_antispam.py
+++ lib/driver/combot_antispam.py
@@ -1,8 +1,5 @@
 import requests
-
 from pyrogram import Client, filters
-from pyrogram.types import Message
-
 
 
 @Client.on_message(filters.command("check_spam"))
@@ -10,15 +7,17 @@ async def antispam(client, message):
     id = " ".join(message.command[1:])
     api = f"https://api.cas.chat/check?user_id={id}"
     session = requests.Session()
-    req = session.request('get', api)
-    status = req.json()['ok']
+    req = session.request("get", api)
+    status = req.json()["ok"]
     if status == "True":
         try:
-            result = req.json()['result']
-            reason = result['messages']
-            offenses = result['offenses']
-            time_added = result['time_added']
-            await message.reply(f"**User:** `{id}`\n**Is banned:** {offenses}\n**Reason:** `{reason}`\n**Time added:** {time_added}")
+            result = req.json()["result"]
+            reason = result["messages"]
+            offenses = result["offenses"]
+            time_added = result["time_added"]
+            await message.reply(
+                f"**User:** `{id}`\n**Is banned:** {offenses}\n**Reason:** `{reason}`\n**Time added:** {time_added}"
+            )
         except Exception as e:
             await message.reply(e)
     else:
